a5ac3038145c4d5952fb2ff99c46a0ad8db6b51f
Edwin Mutua <odoo@softiqtechnologies.com>
2023-10-09 19:26:17 +0300

    modified height, added stopwatch, and tasks

--- tasks.js
+++ tasks.js
@@ -28,16 +28,16 @@ window.addEventListener('load', () => {
         task_input_el.setAttribute("readonly", "readonly");
 
         const checkbox_el = document.createElement("i");
-        checkbox_el.classList.add('bi', 'bi-circle');
+        checkbox_el.classList.add('fa-solid', 'fa-circle');
         checkbox_el.addEventListener("click", () => {
-            if (checkbox_el.classList.contains('bi-circle')) {
-                checkbox_el.classList.remove('bi-circle');
-                checkbox_el.classList.add('bi-check2-circle');
+            if (checkbox_el.classList.contains('fa-circle')) {
+                checkbox_el.classList.remove('fa-circle');
+                checkbox_el.classList.add('fa-circle-check');
 
                 task_input_el.style.textDecoration = "line-through";
             } else {
-                checkbox_el.classList.remove('bi-check2-circle');
-                checkbox_el.classList.add('bi-circle');
+                checkbox_el.classList.remove('fa-circle-check');
+                checkbox_el.classList.add('fa-circle');
 
                 task_input_el.style.textDecoration = "none";
             }
@@ -53,13 +53,13 @@ window.addEventListener('load', () => {
         const task_edit_el = document.createElement('button');
         task_edit_el.classList.add('edit');
         const edit_icon = document.createElement('i');
-        edit_icon.classList.add('bi', 'bi-pencil-square');
+        edit_icon.classList.add('fa-solid', 'fa-pencil');
         task_edit_el.appendChild(edit_icon);
         
         const task_delete_el = document.createElement('button');
         task_delete_el.classList.add('delete');
         const delete_icon = document.createElement('i');
-        delete_icon.classList.add('bi', 'bi-trash');
+        delete_icon.classList.add('fa-solid', 'fa-trash-can');
         task_delete_el.appendChild(delete_icon);
         
         task_actions_el.appendChild(task_edit_el);
@@ -72,14 +72,14 @@ window.addEventListener('load', () => {
         input.value = '';
         
         task_edit_el.addEventListener('click', (e) => {
-          if (edit_icon.classList.contains('bi-pencil-square')) {
-            edit_icon.classList.remove('bi-pencil-square');
-            edit_icon.classList.add('bi-check-square');
+          if (edit_icon.classList.contains('fa-pencil')) {
+            edit_icon.classList.remove('fa-pencil');
+            edit_icon.classList.add('fa-check');
             task_input_el.removeAttribute('readonly');
             task_input_el.focus();
           } else {
-            edit_icon.classList.remove('bi-check-square');
-            edit_icon.classList.add('bi-pencil-square');
+            edit_icon.classList.remove('fa-check');
+            edit_icon.classList.add('fa-pencil');
             task_input_el.setAttribute('readonly', 'readonly');
           }
         });
